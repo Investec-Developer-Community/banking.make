b8ed16c9e7555b837e72ecd0a4bbb807be623e6f
Darryl Daniel <darrylndaniel@gmail.com>
2022-11-10 18:25:15 +0000

    :sparkles: add the ability to configure a blocklist or allowlist on the card policy

--- modules/root_code_card_policy.js
+++ modules/root_code_card_policy.js
@@ -1,9 +1,27 @@
+function getListFromCommaSeparatedString(commaSeparatedString) {
+  if (!commaSeparatedString) {
+    return [];
+  }
+  return commaSeparatedString
+    .split(",")
+    .map(s => s.trim());
+}
+
 module.exports = {
 
   // Converts simple flags into a JS bundle to deploy as RootCode
-  build(sendEvents, approveTransactions, username, partition) {
+  build(
+    sendEvents,
+    approveTransactions,
+    username,
+    partition,
+    allowlist,
+    blocklist
+  ) {
     let token = username
     if (partition) { token += `/${partition}` }
+    const allowlistString = JSON.stringify(getListFromCommaSeparatedString(allowlist));
+    const blocklistString = JSON.stringify(getListFromCommaSeparatedString(blocklist));
     return `
 const config = {
   approveTransactions: ${approveTransactions},
@@ -11,8 +29,24 @@ const config = {
   eventUrl: "https://${process.env.DOMAIN}/investec/special/cards/events"
 }
 
+const verifyMerchantIsAllowed = (merchantCode) => {
+  if (${allowlistString}.length === 0) {
+    return true;
+  }
+  return ${allowlistString}.indexOf(merchantCode) !== -1;
+}
+
+const verifyMerchantIsNotBlocked = (merchantCode) => {
+  if (${blocklistString}.length === 0) {
+    return true;
+  }
+  return ${blocklistString}.indexOf(merchantCode) === -1;
+}
+
 const beforeTransaction = async (transaction) => {
   return config.approveTransactions
+    && verifyMerchantIsAllowed(transaction.merchant.category.code)
+    && verifyMerchantIsNotBlocked(transaction.merchant.category.code);
 };
 
 const afterTransaction = async (transaction) => {
@@ -42,3 +76,4 @@ const Hub = {
   `
   },
 }
+
